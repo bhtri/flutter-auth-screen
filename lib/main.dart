8ebf938fbbc9afd0afbccdd8db50004f17c9e778
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2021-03-21 10:51:55 +0900

    sigin screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:auth_screen/constants.dart';
+import 'package:auth_screen/signin_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,13 +16,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
         primaryColor: kPrimaryColor,
-        // scaffoldBackgroundColor: kBackgroundColor,
+        scaffoldBackgroundColor: kBackgroundColor,
         textTheme: TextTheme(
           headline4:
               TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           button: TextStyle(color: kPrimaryColor),
           headline5:
               TextStyle(color: Colors.white, fontWeight: FontWeight.normal),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.white.withOpacity(0.1),
+            ),
+          ),
         ),
       ),
       home: WelcomeScreen(),
@@ -42,7 +50,7 @@ class WelcomeScreen extends StatelessWidget {
               decoration: BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage('assets/img.jpg'),
-                  // fit: BoxFit.cover,
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
@@ -67,27 +75,40 @@ class WelcomeScreen extends StatelessWidget {
                   ),
                 ),
                 FittedBox(
-                  child: Container(
-                    margin: EdgeInsets.only(bottom: 25),
-                    padding: EdgeInsets.symmetric(horizontal: 26, vertical: 16),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(25),
-                      color: kPrimaryColor,
-                    ),
-                    child: Row(
-                      children: [
-                        Text(
-                          'START LEARNING',
-                          style: Theme.of(context).textTheme.button.copyWith(
-                                color: Colors.black,
-                              ),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return SignInScreen();
+                          },
                         ),
-                        SizedBox(width: 10),
-                        Icon(
-                          Icons.arrow_forward,
-                          color: Colors.black,
-                        )
-                      ],
+                      );
+                    },
+                    child: Container(
+                      margin: EdgeInsets.only(bottom: 25),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 26, vertical: 16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        color: kPrimaryColor,
+                      ),
+                      child: Row(
+                        children: [
+                          Text(
+                            'START LEARNING',
+                            style: Theme.of(context).textTheme.button.copyWith(
+                                  color: Colors.black,
+                                ),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(
+                            Icons.arrow_forward,
+                            color: Colors.black,
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
